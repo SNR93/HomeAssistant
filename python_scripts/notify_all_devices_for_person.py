cd5df99fa4365e90e7a89d72e382dcf766766aa7
SNR93 <48864205+SNR93@users.noreply.github.com>
2025-07-09 10:16:43 +0300

    1
    Отправка уведомлений на все телефоны определенного пользователя HA, а не на какое-то конкретное.
    Способ установки: Home Assistant OS
    Core: 2025.7.1
    Supervisor: 2025.07.1
    Operating System: 15.2

---
+++ python_scripts/notify_all_devices_for_person.py
@@ -0,0 +1,36 @@
+# пример вызова службы
+#service: python_script.notify_aleksei
+#data:
+#  person_entity_id: person.aleksei
+#  message: "Обнаружен дым!"
+#  title: "Критическое оповещение!"
+
+
+
+person_id = data.get('person_entity_id', 'person.aleksei')
+message = data.get('message', 'Тестовое уведомление')
+title = data.get('title', 'Уведомление')
+
+# Получение списка device_trackers
+devices = hass.states.get(person_id).attributes.get('device_trackers', [])
+notify_services = [f"mobile_app_{device.split('.')[-1]}" for device in devices if device.startswith('device_tracker.')]
+
+# Отправка уведомления на каждую службу
+for service in notify_services:
+    hass.services.call('notify', service, {
+        'message': message,
+        'title': title,
+        'data': {
+            'ttl': 0,
+            'priority': 'high',
+            'push': {
+                'sound': {
+                    'name': 'default',
+                    'critical': 1,
+                    'volume': 1.0
+                }
+            },
+            'channel': 'alarm_notifications',  # Для Android
+            'notification_category': 'alarm_notifications'  # Для iOS
+        }
+    })
